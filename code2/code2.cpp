16cd1a571b89b9001b31091734c87c521ebd2f99
KatherXc <4b3g0068@office.stust.edu.tw>
2024-11-13 02:26:04 +0800

    重構迴圈邏輯並修正輸出訊息
    將 #include <iostream> 改為 #include <iostream> using namespace std;
    將原本的 for 迴圈改為新的 for 迴圈，並修正縮排
    新的 for 迴圈從 2 開始，每次遞增 2，計算小於等於 n 的所有偶數和
    新增 while 迴圈，從 2 開始，每次遞增 2，計算小於等於 n 的所有偶數和
    新增 do-while 迴圈，從 2 開始，每次遞增 2，計算小於等於 n 的所有偶數和
    移除原本的 for 迴圈、while 迴圈和 do-while 迴圈
    修正輸出訊息，改為中文描述
    新增 return 0; 作為 main 函數的結尾

--- code2/code2.cpp
+++ code2/code2.cpp
@@ -1,39 +1,37 @@
-﻿﻿#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
-	int n = 1;
-	int sum = 0;
-	cout << "請輸入一個正整數n: ";
-	cin >> n;
+    int n = 1;
+    int sum = 0;
+    cout << "請輸入一個正整數n: ";
+    cin >> n;
 
-	// for loop
-	for (int i = 1; i <= n; i++) {
-		if (i % 2 == 0) sum += i;
-	}
+    // 使用 for 迴圈
+    sum = 0;
+    for (int i = 2; i <= n; i += 2) {
+        sum += i;
+    }
+    cout << "For 迴圈: 小於等於 " << n << " 的所有偶數和為: " << sum << endl;
 
-	sum = 0;
-	for (int i = 2; i <= n; i += 2) {
-		sum += i;
-	}
-	cout << "For loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+    // 使用 while 迴圈
+    int i = 2;
+    sum = 0;
+    while (i <= n) {
+        sum += i;
+        i += 2;
+    }
+    cout << "While 迴圈: 小於等於 " << n << " 的所有偶數和為: " << sum << endl;
 
-	int i = 1;
-	sum = 0;
-	while (i <= n) {
-		if (i % 2 == 0) sum += i;
-		i++;
-	}
-	cout << "While loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
+    // 使用 do-while 迴圈
+    i = 2;
+    sum = 0;
+    do {
+        sum += i;
+        i += 2;
+    } while (i <= n);
+    cout << "Do-While 迴圈: 小於等於 " << n << " 的所有偶數和為: " << sum << endl;
 
-	// do-while loop
-	i = 2;
-	sum = 0;
-	do {
-		sum += i;
-		i += 2;
-	} while (i <= n);
-	cout << "Do-While loop: 1~" << n << "之間所有偶數的和為: " << sum << endl;
-
+    return 0;
 }
